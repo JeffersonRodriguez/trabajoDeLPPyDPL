0b7209c48a9bed48011995ce71494434ffe6a1eb
eduar15 <53593069+eduar15@users.noreply.github.com>
2019-08-27 21:59:49 -0500

    Update programa1.cpp

--- programa1.cpp
+++ programa1.cpp
@@ -1,3 +1,4 @@
+//impresion de frases
 #include<iostream>
 #include<conio.h>
 #include<cstdlib>  
@@ -7,6 +8,48 @@ main()
 	cout<<"buenas noches";
 	cout<<endl;
 	cout<<"es tarde \n"; cout<<"seran las 10";
+	cout<<endl;
+	
+	getch();
+	system("pause");
+}
+
+
+//salto de linea 3 veces
+#include<iostream>
+#include<conio.h>
+#include<cstdlib>  
+using namespace std;
+main()
+{
+	cout<<"buenas noches";
+	cout<<endl;
+	cout<<"es tarde \n\n\n"; cout<<"seran las 10";
+	cout<<endl;
+	
+	getch();
+	system("pause");
+}
+
+
+// suma de numeros e impresion de resultado
+#include<iostream>
+#include<conio.h>
+#include<cstdlib>
+using namespace std;
+main()
+{
+	
+int numero1, numero2, resultado;
+	cout<<"buenas noches";
+	cout<<endl;
+	cout<<"es tarde \n\n\n"; cout<<"seran las 10";
+	cout<<endl;
+	
+	numero1=12;
+	numero2=18;
+	resultado= numero1+numero2;
+	cout<<"\n\n el resultado es:"<< resultado;
 	cout<<endl;
 	
 	getch();
