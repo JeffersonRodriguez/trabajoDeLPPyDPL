371140c18a609fe4e97b0fc368df930f7301bd1d
eduar15 <53593069+eduar15@users.noreply.github.com>
2019-08-29 18:50:54 -0500

    Add files via upload

---
+++ OPERACIONES.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+main()
+{
+	int n1,n2,resp;
+	cout<<endl;
+	cout<<"============================================================================================";
+	cout<<endl;
+	cout<<"Programa para ingresar dos numeros ";
+	cout<<"e imprimir la sumatoria \n\n";
+	cout<<"============================================================================================\n\n";
+	cout<<"Ingrese el primer numero: ";
+	cin>>n1, cout<<"\n";
+	cout<<"Ingrese el segundo numero: ";
+	cin>>n2;
+	cout<<endl;
+	resp=n1+n2;
+	cout<<"El resultado de la suma es: "<< resp, cout<<"\n";
+	
+    cout<<endl;
+	cout<<"============================================================================================";
+	cout<<endl;
+	cout<<"Programa para ingresar dos numeros ";
+	cout<<"e imprimir la resta \n\n";
+	cout<<"============================================================================================\n\n";
+	cout<<"Ingrese el primer numero: ";
+	cin>>n1, cout<<"\n";
+	cout<<"Ingrese el segundo numero: ";
+	cin>>n2;
+	cout<<endl;
+	resp=n1-n2;
+	cout<<"El resultado de la resta es: "<< resp, cout<<"\n";
+	cout<<endl;
+
+   cout<<endl;
+	cout<<"============================================================================================";
+	cout<<endl;
+	cout<<"Programa para ingresar dos numeros ";
+	cout<<"e imprimir la multiplicacion \n\n";
+	cout<<"============================================================================================\n\n";
+	cout<<"Ingrese el primer numero: ";
+	cin>>n1, cout<<"\n";
+	cout<<"Ingrese el segundo numero: ";
+	cin>>n2;
+	cout<<endl;
+	resp=n1*n2;
+	cout<<"El resultado de la multiplicacion es: "<< resp, cout<<"\n";
+	cout<<endl;
+	
+	cout<<endl;
+	cout<<"============================================================================================";
+	cout<<endl;
+	cout<<"Programa para ingresar dos numeros ";
+	cout<<"e imprimir la division \n\n";
+	cout<<"============================================================================================\n\n";
+	cout<<"Ingrese el primer numero: ";
+	cin>>n1, cout<<"\n";
+	cout<<"Ingrese el segundo numero: ";
+	cin>>n2;
+	cout<<endl;
+	resp=n1/n2;
+	cout<<"El resultado de la division es: "<< resp, cout<<"\n";
+	cout<<endl;
+	system("pause");
+}
+
